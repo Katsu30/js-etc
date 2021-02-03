aab15f55369f7a624c6ca02e18a4a0d6373d531c
Katsuhiro <sc30.katsu@gmail.com>
2021-02-03 22:40:26 +0900

    .wip drag and drop components

--- sample_dd/src/App.tsx
+++ sample_dd/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 import './App.css';
 
@@ -9,18 +10,33 @@ function App() {
   return (
     <div className="App">
       <h1>Draggable images</h1>
-      <ul className="images">
-        { IMAGES.map(({ id, name, thumb }) => {
-          return (
-            <li key={ id }>
-              <div className="images-thumb">
-                <img src={ thumb } alt={ `${ name } Thumb` } />
-              </div>
-              <p>{ name }</p>
-            </li>
-          )
-        }) }
-      </ul>
+      <DragDropContext>
+        <Droppable droppableId="images">
+          {(provided) => (
+            <ul className="images" {...provided.droppableProps} ref={provided.innerRef}>
+              { IMAGES.map(({ id, name, thumb }, index) => {
+                return (
+                  <Draggable key={id} draggableId={id} index={index}>
+                  {(provided) => (
+                    <li
+                      key={ id }
+                      ref="{provided.innerRef}"
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <div className="images-thumb">
+                        <img src={ thumb } alt={ `${ name } Thumb` } />
+                      </div>
+                      <p>{ name }</p>
+                    </li>
+                  )}
+                  </Draggable>
+                )
+              }) }
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
